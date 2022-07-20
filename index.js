e6985c22b97598cfc168d4a4958b3381facb3126
Gerard Ngabo <ngabongwi5@hotmail.com>
2022-07-20 21:16:26 +0200

    added Xylophone sound

---
+++ index.js
@@ -0,0 +1,61 @@
+let btnArray = document.querySelectorAll(".drum");
+
+for (let i = 0; i < btnArray.length; i++) {
+  btnArray[i].addEventListener("click", function () {
+    makeSound(btnArray[i].innerHTML)
+    buttonAnimation(btnArray[i].innerHTML)
+  });
+}
+
+document.addEventListener("keypress", function (e) {
+    makeSound(e.key);
+    buttonAnimation(e.key);
+  });
+
+function makeSound(e) {
+  switch (e) {
+    case "w":
+      var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+    case "x":
+      var audio = new Audio("./sounds/Xylophone.mp3");
+      audio.play();
+      break;
+    default:
+      break;
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("."+ currentKey);
+    activeButton.classList.add("pressed")
+    setTimeout(function (){
+        activeButton.classList.remove("pressed")
+    },100)
+}
